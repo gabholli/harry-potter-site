3e742260b761f7436998efc47a7784eec652fa8b
Gabe Hollifield <gholl467@gmail.com>
2025-11-16 18:28:27 -0800

    Changed font and set background to fixed

--- app/routes/siteLayout.tsx
+++ app/routes/siteLayout.tsx
@@ -6,7 +6,7 @@ const siteLayout = () => {
         <div className="grid grid-cols-1 grid-rows-[125px_1fr] min-h-dvh
             lg:grid-cols-[200px_1fr] lg:grid-rows-1 3xl:grid-cols-[400px_1fr]
             bg-[url(/black-background.jpg)] bg-center bg-no-repeat bg-cover
-            bg-black/50 bg-blend-overlay">
+            bg-black/50 bg-blend-overlay bg-fixed">
             <NavMenu />
             <Outlet />
         </div>
